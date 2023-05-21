f828302b2b855e5ba4a05b2ee77fcff4203319d6
rakhat_cyanid <zhayshilik@gmail.com>
2023-05-21 14:52:57 +0600

    BTS Initialize

---
+++ BST.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class BST<K extends Comparable<K>, V>  {
+  private Node root;
+  private int size = 0;
+  public class Node{
+    private K key;
+    private V value;
+    private Node left, right;
+    public Node(K key, V value) {
+      this.key = key;
+      this.value = value;
+    }
+
+    @Override
+    public String toString() {
+      return "{key: " + this.key + " value: " + this.value + "}";
+    }
+  }
+}
