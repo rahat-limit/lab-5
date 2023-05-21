fc7f08949d271a0cb34144c6d1efc3ef194623a3
rakhat_cyanid <zhayshilik@gmail.com>
2023-05-21 15:14:39 +0600

    BST Main Realization

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
   public static void main(String[] args) {
+    BST<Integer, String> bst = new BST();
 
+    for (int i = 0; i < 12; i++) {
+      bst.put(i, "Val " + i);
+    }
+    Iterable it = bst.iterator();
+    for(Object el: it) {
+      System.out.println(el);
+    }
   }
 }
